8988b5cd522aa640ae5263eb7abc14541a458b0f
MAS-991 <145535946+MAS-991@users.noreply.github.com>
2025-06-05 09:13:22 +0800

    add basic math functions: add,subtract,multiply,divide

---
+++ script.js
@@ -0,0 +1,20 @@
+// [ ] { }
+function add(a , b){
+    return a + b ; 
+};
+
+function subtraction(a , b){ 
+    return a - b;
+};
+
+function multiply (a , b){ 
+    return a * b ; 
+
+};
+
+function divide (a , b){ 
+    if (b===0)
+        return "cannot divide by zero";
+    return a / b ;
+};
+
